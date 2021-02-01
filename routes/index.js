a9bd4844882495bed4729cbc36405f80bc458cde
Simon Gottschlag <simon.gottschlag@xenit.se>
2021-02-01 20:24:32 +0100

    Test 3

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-  res.render('index', { title: 'TestAbc123' });
+  res.render('index', { title: 'Test3' });
 });
 
 module.exports = router;

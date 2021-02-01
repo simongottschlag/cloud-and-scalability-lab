6f5bb011c2673569bcd7e433c3cd1fbbaea7f8ea
Simon Gottschlag <simon.gottschlag@xenit.se>
2021-02-02 00:00:39 +0100

    test

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-  res.render('index', { title: 'ContainersAreAwsome' });
+  res.render('index', { title: 'AzureLab' });
 });
 
 module.exports = router;

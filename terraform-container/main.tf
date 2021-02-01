62eb282e88a387a8fac030aab97eff1ee0e2678a
Simon Gottschlag <simon.gottschlag@xenit.se>
2021-02-01 22:54:51 +0100

    add terraform-container

---
+++ terraform-container/main.tf
@@ -0,0 +1,126 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "2.45.1"
+    }
+    github = {
+      source  = "integrations/github"
+      version = "4.3.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+provider "github" {
+  token = var.github_token
+  owner = var.github_owner
+}
+
+locals {
+  resource_group_name           = "rg-${var.environment}-${var.location}-${var.name}"
+  app_service_plan_name         = "asp-${var.environment}-${var.location}-${var.name}"
+  app_service_name              = "wa-${var.environment}-${var.location}-${var.name}"
+  azure_container_registry_name = "acr${var.environment}${var.location}${var.name}"
+}
+
+resource "azurerm_resource_group" "this" {
+  name     = local.resource_group_name
+  location = var.location_long
+}
+
+resource "azurerm_app_service_plan" "this" {
+  name                = local.app_service_plan_name
+  location            = azurerm_resource_group.this.location
+  resource_group_name = azurerm_resource_group.this.name
+  kind                = "Linux"
+  reserved            = true
+
+  sku {
+    tier = "Basic"
+    size = "B1"
+  }
+}
+
+resource "azurerm_app_service" "this" {
+  name                = local.app_service_name
+  location            = azurerm_resource_group.this.location
+  resource_group_name = azurerm_resource_group.this.name
+  app_service_plan_id = azurerm_app_service_plan.this.id
+
+  site_config {
+    app_command_line = ""
+    linux_fx_version = "DOCKER|${azurerm_container_registry.this.login_server}/${var.container_name}:latest"
+  }
+
+  identity {
+    type = "SystemAssigned"
+  }
+
+  app_settings = {
+    "WEBSITES_ENABLE_APP_SERVICE_STORAGE" = "false"
+    "DOCKER_REGISTRY_SERVER_URL"          = "https://${azurerm_container_registry.this.login_server}"
+  }
+
+  lifecycle {
+    ignore_changes = [
+      site_config[0].linux_fx_version,
+    ]
+  }
+}
+
+resource "azurerm_container_registry" "this" {
+  name                = local.azure_container_registry_name
+  resource_group_name = azurerm_resource_group.this.name
+  location            = azurerm_resource_group.this.location
+  sku                 = "Basic"
+  admin_enabled       = true
+}
+
+resource "azurerm_role_assignment" "this" {
+  scope                = azurerm_container_registry.this.id
+  role_definition_name = "AcrPull"
+  principal_id         = azurerm_app_service.this.identity[0].principal_id
+}
+
+resource "azurerm_container_registry_webhook" "this" {
+  name                = replace(azurerm_app_service.this.name, "-", "")
+  registry_name       = azurerm_container_registry.this.name
+  resource_group_name = azurerm_resource_group.this.name
+  location            = azurerm_resource_group.this.location
+
+  service_uri = "https://${azurerm_app_service.this.site_credential[0].username}:${azurerm_app_service.this.site_credential[0].password}@${azurerm_app_service.this.name}.scm.azurewebsites.net/docker/hook"
+  status      = "enabled"
+  scope       = "${var.container_name}:*"
+  actions     = ["push"]
+  custom_headers = {
+    "Content-Type" = "application/json"
+  }
+}
+
+resource "github_actions_secret" "acr_username" {
+  repository      = var.github_repository
+  secret_name     = "ACR_USERNAME"
+  plaintext_value = azurerm_container_registry.this.admin_username
+}
+
+resource "github_actions_secret" "acr_password" {
+  repository      = var.github_repository
+  secret_name     = "ACR_PASSWORD"
+  plaintext_value = azurerm_container_registry.this.admin_password
+}
+
+resource "github_actions_secret" "acr_hostname" {
+  repository      = var.github_repository
+  secret_name     = "ACR_HOSTNAME"
+  plaintext_value = azurerm_container_registry.this.login_server
+}
+
+resource "github_actions_secret" "container_name" {
+  repository      = var.github_repository
+  secret_name     = "CONTAINER_NAME"
+  plaintext_value = var.container_name
+}
---
+++ terraform-container/variables.tf
@@ -0,0 +1,46 @@
+variable "environment" {
+  description = "The environment short name"
+  type        = string
+  default     = "lab"
+}
+
+variable "location_long" {
+  description = "The location where to place resources"
+  type        = string
+  default     = "west europe"
+}
+
+variable "location" {
+  description = "The location short name"
+  type        = string
+  default     = "we"
+}
+
+variable "name" {
+  description = "The name to use for the different parts of the deployment"
+  type        = string
+  default     = "webapp1"
+}
+
+variable "container_name" {
+  description = "The name of the container that will be deployed to the Azure Web App"
+  type        = string
+  default     = "azure-lab"
+}
+
+variable "github_repository" {
+  description = "The GitHub repository to use"
+  type        = string
+}
+
+variable "github_owner" {
+  description = "The GitHub username to use (Use the following environment variable to inject: TF_VAR_github_owner)"
+  type        = string
+  sensitive   = true
+}
+
+variable "github_token" {
+  description = "The GitHub token to use (Use the following environment variable to inject: TF_VAR_github_token)"
+  type        = string
+  sensitive   = true
+}

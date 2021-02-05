1e4bbc554d1c553645fc68eff0b91fd8d8ae18f7
Simon Gottschlag <simon.gottschlag@xenit.se>
2021-02-05 15:44:58 +0100

    Update title

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-  res.render('index', { title: 'Woopidoo' });
+  res.render('index', { title: 'AzureLabz' });
 });
 
 module.exports = router;
